f1c1f26e40ab0821e5cb3c0668ee005aac2099f2
Djohny-coder <jonathandiasso67@gmail.com>
2026-04-14 12:29:58 +0000

    connect frontend to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: "https://gestfin-backend.onrender.com/api/" });
 
 API.interceptors.request.use(cfg => {
   const token = localStorage.getItem('access_token');
